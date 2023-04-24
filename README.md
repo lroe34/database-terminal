da350ec06bd491a93dd7d3cc19b051dbc1f371c2
lroe34 <101296666+lroe34@users.noreply.github.com>
2023-04-23 23:17:36 -0500

    added execute code

--- README.md
+++ README.md
@@ -1,1 +1,26 @@
 # database-terminal
+
+Although I have put over 20 hours into this assignment over the course of the past few weeks, there still a few missing assignment criteria that I would like to recognize. The first is that, while the terminal can easily read databases and tables that are already availible on MySQL server, (say created through MySQL Workbench) it has a tough time creating tables that include procedures and functions. I believe I have narrowed down the root of this. I think that the 'DELIMITER' function which is built into MySQL is not native to sql.connector and, as such, cannot run through the connector. I have tried doing this as a multiline query as well with no luck. Since "Sakila-schema.sql" has special delimiters in it, ("//", "$$", etc.) it displays this behavior when being processed by the terminal. I have come to this conclusion after about 7 hours of bug-squashing and forum roaming and I have a fuzzy idea of what a work-around might look like:
+
+```
+import re   //for regex
+
+function remove_delimiters.pseudo{
+  lines = re.split(r";\n|;;\n|\$\$\n|//\n",sqlFile) //separate by '//', '$$', ';' and ';;'
+  for line in lines:
+    if line type == func or proc
+        cursor.execute(line, multi=True)
+        #somehow set the delimiter to work
+    else:
+        cursor.execute(line)
+}
+```
+
+Furthermore, the drop tables function works, but it shouldbe noted that it will make a huge mess if the databases that it drops tables from. This is because it drops the table regardless of any keys that it may have which breaks tables that rely on it for foreign keys. I was going to implement a check from the INFORMATION_SCHEMA.TABLE_CONSTRAINTS table to more gracefully handle this, but unfortunetly I ran out of time (the beginnings of my code for this are evident in app.py). On this note, I was going to implement a feature to filter the table the user is viewing by a few user adjusted parameters, but also ran out of time for this feature. 
+
+## Things I wish I had more time for:
+- delimiter work-around 
+- filter feature
+- graceful drop table
+- cleaner UI
+- document code better
--- app.py
+++ app.py
@@ -1,52 +1,130 @@
 from flask import Flask, render_template,request,redirect, url_for
+from sqlite3 import OperationalError
 from flask_sqlalchemy import SQLAlchemy
 import mysql.connector
+import re
+
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = f'mysql+pymysql://root:mike2002@localhost'
-db = SQLAlchemy(app)
-
-mydb = mysql.connector.connect(
-  host="localhost",
-  user="root",
-  password="mike2002",
-)
 
 
-mycursor = mydb.cursor()
 
-mycursor.execute("SELECT SCHEMA_NAME FROM INFORMATION_SCHEMA.SCHEMATA;")
+#mydb = mysql.connector.connect(
+#  host="localhost",
+#  user="root",
+#  password="mike2002",
+#)
 
-myresult = mycursor.fetchall()
-available = []
-for database in myresult:
-  print(database[0])
-  tables = []
-  if database[0] not in ['mysql', 'information_schema', 'performance_schema','sys']:
-    mycursor.execute(f"USE {database[0]}") # select the database
-    mycursor.execute("SHOW TABLES")
-    for (table_name,) in mycursor:
-          tables.append(table_name)
-    available.append((database[0],tables))
+username= None
+password= None
+host= None
+port= None
 
-print(available)
 
-mycursor.execute("SELECT SCHEMA_NAME FROM INFORMATION_SCHEMA.SCHEMATA;")
+#mycursor = mydb.cursor()
+#
+#mycursor.execute("SELECT SCHEMA_NAME FROM INFORMATION_SCHEMA.SCHEMATA;")
+#
+#myresult = mycursor.fetchall()
+#available = []
+#for database in myresult:
+#  tables = []
+#  if database[0] not in ['mysql', 'information_schema', 'performance_schema','sys']:
+#    mycursor.execute(f"USE {database[0]}") # select the database
+#    mycursor.execute("SHOW TABLES")
+#    for (table_name,) in mycursor:
+#          tables.append(table_name)
+#    available.append((database[0],tables))
+#
+#mycursor.execute("SELECT SCHEMA_NAME FROM INFORMATION_SCHEMA.SCHEMATA;")
+#
+#myresult = mycursor.fetchall()
 
-myresult = mycursor.fetchall()
-
+#print(available)
 
 @app.route('/login', methods=['POST',"GET"])
 def home():
     # Get the form data
-    username = request.form['username']
-    password = request.form['password']
-    host = request.form['host']
-    port = request.form['port']
+    global username
+    global host
+    global port
+    global password
+    filename = None
+    try:
 
+      host = request.form['host']
+      username = request.form['username']
+      password = request.form['password']
+      port = request.form['port']
+    except:
+      print(request.form)
+      data  = request.form
+      value = data.getlist('value')[0]
+      print('value:', value)
+      value = value.split(",")
+      filename = value[0]
+      print(filename)
+
+
+    app.config['SQLALCHEMY_DATABASE_URI'] = f'mysql+pymysql://{host}:{password}@{username}'
+
+    mydb = mysql.connector.connect(
+      host=host,
+      user=username,
+      password=password,
+      port = port
+    )
+    mycursor = mydb.cursor()
+
+    mycursor.execute("SELECT SCHEMA_NAME FROM INFORMATION_SCHEMA.SCHEMATA;")
+
+    myresult = mycursor.fetchall()
+    available = []
+    for database in myresult:
+      tables = []
+      if database[0] not in ['mysql', 'information_schema', 'performance_schema','sys']:
+        mycursor.execute(f"USE {database[0]}") # select the database
+        mycursor.execute("SHOW TABLES")
+        for (table_name,) in mycursor:
+              tables.append(table_name)
+        available.append((database[0],tables))
+
+    mycursor.execute("SELECT SCHEMA_NAME FROM INFORMATION_SCHEMA.SCHEMATA;")
+
+    myresult = mycursor.fetchall()
     # Connect to MySQL database using the provided host and port
     # and check if username and password match your database records
-    
+    # Open and read the file as a single buffer
+
+    if filename:
+      fd = open(filename, 'r', encoding="utf-8")
+      sqlFile = fd.read()
+      fd.close()
+
+      # all SQL commands (split on ';')
+      sqlCommands = sqlFile.split(';\n')
+      #sqlCommands = re.split(r";\n|;;\n|\$\$\n|//\n",sqlFile)
+      #print(sqlCommands[0])
+      #for block in sqlCommands:
+      #  vers = block[0:4].replace(' ','')
+      #  vers = vers.replace('\n','')
+      #  if vers == ';' or "-":
+      #     
+      #     lines = block.split(';\n')
+      #     for line in lines:
+      #        print("line:", line)
+      #        mycursor.execute(line)
+      #  print('-----------------------------------------------------------------')
+#
+             
+      # Execute every command from the input file
+      for command in sqlCommands:
+          try:
+              print(command)
+              mycursor.execute(command, multi=True)
+          except mysql.connector.Error as err:
+              print(F"Error: {err}")
+      
     return render_template('index.html',myresult=myresult, username=username, host=host, port=port,available=available)
 
 @app.route('/')
@@ -58,7 +136,11 @@ def login():
 
 @app.route('/database', methods=['POST'])
 def view_table():
-    
+    global username
+    global host
+    global port
+    global password
+    print(request.form)
     data  = request.form
     value = data.getlist('value')[0]
     value = value.split(",")
@@ -66,24 +148,107 @@ def view_table():
     table = value[1]
 
     mydb = mysql.connector.connect(
-    host="localhost",
-    user="root",
-    password="mike2002",
-    database = db
+      host=host,
+      user=username,
+      password=password,
+      database = db
     )
     mycursor = mydb.cursor()
+    print(mycursor)
+    print(db, table)
     mycursor.execute(f"SELECT * FROM {table} LIMIT 50;")
 
     myresult = mycursor.fetchall()
 
+    field_names = [i[0] for i in mycursor.description]
+    print(field_names)
     print('Showing '+db+'.'+table)
 
-    return render_template('database.html',myresult= myresult,table=table, )
+    return render_template('database.html',myresult= myresult,table=table, field_names=field_names, db=db)
+
+@app.route('/new', methods=['POST'])
+def new():
+    return render_template('new.html')
+
+@app.route('/show', methods=['POST'])
+def show():
+    
+    global username
+    global host
+    global port
+    global password
+    mydb = mysql.connector.connect(
+      host=host,
+      user=username,
+      password=password
+    )
+    mycursor = mydb.cursor()
+    
+    for x in request.form:
+      if x == 'filename':
+        filename = request.form['filename']
+    if filename:
+      fd = open(filename, 'r', encoding="utf-8")
+      sqlFile = fd.read()
+      fd.close()
+
+      # all SQL commands (split on ';')
+      sqlCommands = sqlFile.split(';\n')
+      #print(sqlCommands)
+      # Execute every command from the input file
+      #for command in sqlCommands:
+      # print(command)
+      # try:
+      #     print(command)
+      #     mycursor.execute(command)
+      # except OperationalError:
+      #     print(F"Command skipped: {command}")
+  
+    return render_template('show.html', doc=sqlCommands ,filename=filename, username=username, host=host,port=port,password=password)
 
 @app.route('/table_menu', methods=['POST'])
 def table_menu():
     action  = request.form
-    value = action.getlist(0)
-    print(value)
-    mycursor.execute("DROP TABLE IF EXISTS EMPLOYEE")
-    return value
+    val = action.getlist('value')[0]
+    val = val.split(',')
+    db = val[2]
+    global username
+    global host
+    global port
+    global password
+    mydb = mysql.connector.connect(
+      host=host,
+      user=username,
+      password=password,
+      database = db
+    )
+    if val[0] == 'drop':
+
+      
+      mycursor = mydb.cursor()
+
+      mycursor.execute(f"SELECT CONSTRAINT_NAME FROM INFORMATION_SCHEMA.TABLE_CONSTRAINTS WHERE TABLE_NAME='{val[1]}';")
+      constaints = mycursor.fetchall()
+      temp = []
+      for constraint in constaints:
+         
+         if constraint[0] != 'PRIMARY':
+          temp.append(constraint[0])
+      
+      print(temp)
+
+      for x in temp:
+        mycursor.execute(f'ALTER TABLE {val[1]} DROP FOREIGN KEY {x};')
+
+      mycursor.execute(f"SET foreign_key_checks = 0;")
+      mycursor.execute(f"DROP TABLE IF EXISTS {val[1]}")
+      mycursor.execute(f"SET foreign_key_checks = 1;")
+ 
+      mycursor.execute("SHOW Tables")
+      print(mycursor.fetchall())
+      return val
+
+    if val[0] == 'filter':
+
+      return val
+    
